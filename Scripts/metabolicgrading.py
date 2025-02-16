3e154bf193c9cfb25afb10549392c04a244ffe33
ashertwang <ashertwang@gmail.com>
2025-02-16 12:32:41 -0500

    Update metabolicgrading.py

--- Scripts/metabolicgrading.py
+++ Scripts/metabolicgrading.py
@@ -17,7 +17,7 @@ from sklearn.metrics import accuracy_score, confusion_matrix, classification_rep
 from sklearn.inspection import permutation_importance
 from imblearn.over_sampling import ADASYN
 
-data = pd.read_csv("mg_unadjmetaboliteprofiling.csv")
+data = pd.read_csv("../Data/mg_unadjmetaboliteprofiling.csv")
 features = data.columns[2:]
 target_column = 'Tumor Grade'
 
